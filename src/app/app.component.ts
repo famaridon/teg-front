db9d7f9ba8062f2eff6fa5f6f17f7ac4bb996d9d
famaridon <famaridon@users.noreply.github.com>
2019-10-02 13:25:36 +0200

    remove stom ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TegService } from './services/teg.service';
-import { Observable } from 'rxjs';
-import { RxStompService } from '@stomp/ng2-stompjs';
-import { Message } from '@stomp/stompjs';
 
 @Component({
   selector: 'app-root',
